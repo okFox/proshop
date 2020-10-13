e86dd4c071c95f2c1e80ba04c2e0edc153dc09c1
okFox <smayfield@gmail.com>
2020-10-13 00:15:28 -0700

    mapping working correctly

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -9,7 +9,7 @@ const Homescreen = () => {
            <Row>
             {products.map(product => (
                 <Col sm={12} md={6} lg={4} xl={3}>
-                    <h3>{products.name}</h3>
+                    <h3>{product.name}</h3>
                 </Col>
             ))}
            </Row>

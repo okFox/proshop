06c7f60e1b05afaba82e7febe9a71327d2a29c3d
okFox <smayfield@gmail.com>
2020-10-13 22:09:44 -0700

    cleaned up manifest.json to remove errors in console

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import PropTypes from 'prop-types'
 
 
 const Product = ({ product}) => {

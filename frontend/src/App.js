b7ea31295088821e3efea628e475c63437847f62
okFox <smayfield@gmail.com>
2020-10-22 22:54:33 -0700

    added home and productID routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Homescreen from "./screens/Homescreen"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
-    <>
-      < Header />
+    <Router>
+      <Header />
         <main className='py-3'>
           <Container>
-            <Homescreen />
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} exact />
           </Container>
         </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 

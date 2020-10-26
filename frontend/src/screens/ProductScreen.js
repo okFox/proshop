3731a25eca7e608e85f4d201f90115e30714516e
okFox <smayfield@gmail.com>
2020-10-25 22:14:42 -0700

    housekeeping and started product detail

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap' 
+import Rating from '../components/Rating'
+import product from '../products'
+import products from '../products'
 
-const ProductScreen = () => {
+//destructure match off of props.  The id comes from the route in App.js
+const ProductScreen = ({ match }) => {
+    const product = products.find(p => p._id === match.params.id)
     return (
         <div>
-           Product 
+           {product.name} 
         </div>
     )
 }

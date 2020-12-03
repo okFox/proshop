be34448574a3a6fc0fe499c8c3c2389e658850df
okFox <smayfield@gmail.com>
2020-12-02 20:59:50 -0800

    wired to backend server

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+// import products from '../products'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 
 const Homescreen = () => {
+    const [products, setProducts] = useState([])
+
+    //loads when component loads
+    useEffect(() => {
+        //new function in order to make it async/await-able
+        const fetchProducts = async () => {
+            //destructure data off of res object
+            const { data } = await axios.get('/api/products')
+            setProducts(data)
+        }
+        fetchProducts()
+    }, []) //place dependancies in array.  anying you want to fire useEffect off when it changes
+    
     return (
         <>
            <h1>Latest Products</h1>

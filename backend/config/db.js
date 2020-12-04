c72e94d0f758d642c0b8b33b5ea95832fc743650
okFox <smayfield@gmail.com>
2020-12-03 22:17:58 -0800

    connected mongoDB atlas

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+        console.log(`MongoDB connected on ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1) // 1 is node's "uncaught fatal exception" error. node exits with a 0 status code when no more async operations are pending.
+    }
+}
+
+export default connectDB

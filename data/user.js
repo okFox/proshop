f6fbc3776b5f5862ba9bca21b18695c6d3684b2a
okFox <smayfield@gmail.com>
2020-12-13 10:01:04 -0800

    sample users.  removed IDs from product.js to prepare for seeder script

---
+++ data/user.js
@@ -0,0 +1,20 @@
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: xxxxx,
+        isAdmin: true
+    },
+    {
+        name: 'Johnny Silverhand',
+        email: 'john@example.com',
+        password: xxxxx,
+        isAdmin: false
+    },
+    {
+        name: 'Sarah Mayfield',
+        email: 'sarah@example.com',
+        password: xxxxx,
+        isAdmin: false
+    }
+]
